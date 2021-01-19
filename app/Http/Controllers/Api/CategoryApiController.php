0551d599a7d66209f720545d441f83dbc715693a
Diogo Ferreira <diogo@diogofmedeiros.com>
2021-01-19 16:12:38 -0300

    Aula 10 - Módulo APIs no Laravel

---
+++ app/Http/Controllers/Api/CategoryApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\TenantFormRequest;
+use App\Http\Resources\CategoryResource;
+use App\Services\CategoryService;
+
+class CategoryApiController extends Controller
+{
+
+    /**
+     * @var CategoryService
+     */
+    private $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    public function categoriesByTenant(TenantFormRequest $request)
+    {
+        return CategoryResource::collection(
+            $this->categoryService->getCategoriesByUuid($request->token_company)
+        );
+    }
+
+    public function show(TenantFormRequest $request, $url)
+    {
+        if (!$category = $this->categoryService->getCategoryByUrl($url))
+            return response()->json(['message' => 'not Found'], 404);
+
+        return new CategoryResource($category);
+    }
+
+}
